1f16d3fcf333ec3a7fa6c3c47972e135e194326f
Brabant Paul <paul.brabant2007@gmail.com>
2023-06-06 10:05:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("Sidbar").style.width = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("Sidbar").style.width = "0";
+  }
+
+function showFoto(){
+  document.getElementById("foto").style.width = "100%"
+}
